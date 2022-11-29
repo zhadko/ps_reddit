0094dbff251521dc3cf2e3c013e3362f6248882d
corrrvo <nazar.yurie@gmail.com>
2022-11-29 21:13:42 +0200

    Final commit: added serializers and etc., project logic is successfully done.

--- ps_reddit/settings.py
+++ ps_reddit/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0so!b&2tqincx!#a=ckm2oo1ob!e7niwk)+xfug#h)s3$0nudu"
+# Add .env variables anywhere before SECRET_KEY
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
+# UPDATE secret key
+SECRET_KEY = os.environ['SECRET_KEY'] # Instead of your actual secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'posts',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- ps_reddit/urls.py
+++ ps_reddit/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from posts.views import PostList, VoteCreate, PostDetail
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/posts/", PostList.as_view()),
+    path("api/posts/<int:pk>/", PostDetail.as_view()),
+    path("api/posts/<int:pk>/vote/", VoteCreate.as_view()),
+    path("api-auth/", include('rest_framework.urls')),
 ]
